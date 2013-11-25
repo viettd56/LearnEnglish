d768214e0b46ae8aacb3de4b569d0f91de4a5f1f
thaimanhtuan9319 <thaimanhtuan1993@outlook.com>
2013-11-25 18:45:47 +0700

    Add page

--- LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
+++ LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
@@ -5,8 +5,76 @@
 *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 */
 
-loadPage = function(){
+loadAnimal = function(){
 	$("#pagePort").load("pages/Animal.html", function(){currentPage.init();
+	});
+};
+loadFlower = function(){
+	$("#pagePort").load("pages/Flower.html", function(){currentPage.init();
+	});
+};
+loadEntertainment = function(){
+	$("#pagePort").load("pages/Entertainment.html", function(){currentPage.init();
+	});
+};
+loadFood = function(){
+	$("#pagePort").load("pages/Food.html", function(){currentPage.init();
+	});
+};
+loadColor = function(){
+	$("#pagePort").load("pages/Color.html", function(){currentPage.init();
+	});
+};
+loadNature = function(){
+	$("#pagePort").load("pages/Nature.html", function(){currentPage.init();
+	});
+};
+loadSport = function(){
+	$("#pagePort").load("pages/Sport.html", function(){currentPage.init();
+	});
+};
+loadFamily = function(){
+	$("#pagePort").load("pages/Family.html", function(){currentPage.init();
+	});
+};
+loadHealth = function(){
+	$("#pagePort").load("pages/Health.html", function(){currentPage.init();
+	});
+};
+loadMaterial = function(){
+	$("#pagePort").load("pages/Material.html", function(){currentPage.init();
+	});
+};
+loadClothing = function(){
+	$("#pagePort").load("pages/Clothing.html", function(){currentPage.init();
+	});
+};
+loadCity = function(){
+	$("#pagePort").load("pages/City.html", function(){currentPage.init();
+	});
+};
+loadTool = function(){
+	$("#pagePort").load("pages/Tool.html", function(){currentPage.init();
+	});
+};
+loadProfession = function(){
+	$("#pagePort").load("pages/Profession.html", function(){currentPage.init();
+	});
+};
+loadTime = function(){
+	$("#pagePort").load("pages/Time.html", function(){currentPage.init();
+	});
+};
+loadEducation = function(){
+	$("#pagePort").load("pages/Education.html", function(){currentPage.init();
+	});
+};
+loadWork = function(){
+	$("#pagePort").load("pages/Work.html", function(){currentPage.init();
+	});
+};
+loadWeather = function(){
+	$("#pagePort").load("pages/Weather.html", function(){currentPage.init();
 	});
 };
 
