7c8c1e5f5be81845016e304effc3b70a5b86ea39
thaimanhtuan9319 <thaimanhtuan1993@outlook.com>
2013-11-28 19:39:51 +0700

    multipage

--- LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
+++ LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
@@ -1,80 +1,13 @@
-/*
-*  Licensed Materials - Property of IBM
-*  5725-G92 (C) Copyright IBM Corp. 2006, 2013. All Rights Reserved.
-*  US Government Users Restricted Rights - Use, duplication or
-*  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-*/
+currentPage = {};
 
-loadAnimal = function(){
-	$("#pagePort").load("pages/Animal.html", function(){currentPage.init();
+currentPage.init = function(){
+	WL.Logger.debug("MainPage :: init");
+};
+	
+currentPage.loadPage = function(pageIndex){
+	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + pageIndex);
+	pagesHistory.push("pages/MainPage.html");
+	$("#pagePort").load("pages/Page" + pageIndex + ".html", function(){
+		currentPage.init();
 	});
 };
-loadFlower = function(){
-	$("#pagePort").load("pages/Flower.html", function(){currentPage.init();
-	});
-};
-loadEntertainment = function(){
-	$("#pagePort").load("pages/Entertainment.html", function(){currentPage.init();
-	});
-};
-loadFood = function(){
-	$("#pagePort").load("pages/Food.html", function(){currentPage.init();
-	});
-};
-loadColor = function(){
-	$("#pagePort").load("pages/Color.html", function(){currentPage.init();
-	});
-};
-loadNature = function(){
-	$("#pagePort").load("pages/Nature.html", function(){currentPage.init();
-	});
-};
-loadSport = function(){
-	$("#pagePort").load("pages/Sport.html", function(){currentPage.init();
-	});
-};
-loadFamily = function(){
-	$("#pagePort").load("pages/Family.html", function(){currentPage.init();
-	});
-};
-loadHealth = function(){
-	$("#pagePort").load("pages/Health.html", function(){currentPage.init();
-	});
-};
-loadMaterial = function(){
-	$("#pagePort").load("pages/Material.html", function(){currentPage.init();
-	});
-};
-loadClothing = function(){
-	$("#pagePort").load("pages/Clothing.html", function(){currentPage.init();
-	});
-};
-loadCity = function(){
-	$("#pagePort").load("pages/City.html", function(){currentPage.init();
-	});
-};
-loadTool = function(){
-	$("#pagePort").load("pages/Tool.html", function(){currentPage.init();
-	});
-};
-loadProfession = function(){
-	$("#pagePort").load("pages/Profession.html", function(){currentPage.init();
-	});
-};
-loadTime = function(){
-	$("#pagePort").load("pages/Time.html", function(){currentPage.init();
-	});
-};
-loadEducation = function(){
-	$("#pagePort").load("pages/Education.html", function(){currentPage.init();
-	});
-};
-loadWork = function(){
-	$("#pagePort").load("pages/Work.html", function(){currentPage.init();
-	});
-};
-loadWeather = function(){
-	$("#pagePort").load("pages/Weather.html", function(){currentPage.init();
-	});
-};
-
