02a9a8e97a9c0e6dd7dd85e97ddab139a174e2c1
viettd56 <viet751993@gmail.com>
2013-12-01 23:49:44 +0700

    add effect

--- LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
+++ LearnEnglish/apps/LearnEnglish/common/js/MainPage.js
@@ -7,7 +7,11 @@ currentPage.init = function(){
 currentPage.loadPage = function(pageIndex){
 	WL.Logger.debug("MainPage :: loadPage :: pageIndex: " + pageIndex);
 	pagesHistory.push("pages/MainPage.html");
-	$("#pagePort").load("pages/Page" + pageIndex + ".html", function(){
+	var animate = $(".icon").hide(1000);
+	$.when(animate).done(function() {
+		$("#pagePort").load("pages/Page" + pageIndex + ".html", function(){
 		currentPage.init();
+	
+	});
 	});
 };
